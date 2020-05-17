ecb1d5603c50a57ce11be057470c101e9d5705fa
SennaTheRedeemer <ofir.norani2@gmail.com>
2020-05-17 11:27:00 +0300

    Added navbar and sidebar

--- src/App.vue
+++ src/App.vue
@@ -37,16 +37,59 @@
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer
+          permanent
+          expand-on-hover
+        >
+          <v-list>
+            <v-list-item class="px-2 pt-12 mt-4">
+              <v-list-item-avatar>
+                <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+              </v-list-item-avatar>
+            </v-list-item>
+
+            <v-list-item link>
+              <v-list-item-content>
+                <v-list-item-title class="title">Sandra Adams</v-list-item-title>
+                <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list
+            nav
+            dense
+          >
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-folder</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>My Files</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-account-multiple</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Shared with me</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-star</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Starred</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
   </v-app>
 </template>
 
 <script>
-import Menu from './components/Menu';
 
 export default {
   name: 'App',
   components: {
-    Menu,
   },
 
   data: () => ({
@@ -54,3 +97,5 @@ export default {
   }),
 };
 </script>
+<style scoped>
+</style>
