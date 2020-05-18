06209e66e92e8a2e6f2af369268d3bda98f60801
SennaTheRedeemer <ofir.norani2@gmail.com>
2020-05-18 11:57:54 +0300

    refactor displayMails component

--- src/components/DisplayMails.vue
+++ src/components/DisplayMails.vue
@@ -1,7 +1,7 @@
 <template>
     
     <v-mailcard :title="title" :maxHeight="maxHeight" >
-        <v-responsive :min-height="844" v-if="stretch">
+        <v-responsive :min-height="minHeight">
             <v-list class="viewCard">
                 <v-list-item-group v-model="model">
                 <v-list-item
@@ -18,21 +18,6 @@
                 </v-list-item-group>
             </v-list>
       </v-responsive>
-      <v-list class="viewCard" v-else>
-        <v-list-item-group v-model="model">
-        <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
-        >
-            <v-list-item-icon>
-            <v-checkbox :on-icon="item.icon" :off-icon="item.icon" color="#FFD600"></v-checkbox>
-            </v-list-item-icon>
-            <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-            </v-list-item-content>
-        </v-list-item>
-        </v-list-item-group>
-    </v-list>
     </v-mailcard>
     
 </template>
@@ -46,6 +31,16 @@ export default {
         title: String,
         maxHeight: Number,
         stretch: Boolean
+    },
+    computed: {
+        minHeight: function() {
+            if (this.stretch) {
+                return 844;
+            }
+            else {
+                return 0;
+            }
+        }
     },
     components: {
         'v-mailcard': MailCard,
